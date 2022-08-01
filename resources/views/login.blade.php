ad9428c6a39726f7c0a953be924e517695233486
Chompanha <92453740+Chompanha@users.noreply.github.com>
2022-08-01 11:55:22 +0700

    add login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,51 +23,56 @@
 
 
 
-
 <div class="container">
     <div class="row">
 
-        <div class="col-md mt-3 " data-aos="fade-right" style="text-align:center">
+        <div class="col-md" style="text-align:center">
             <img src="pic.png" class="img-responsive" alt="">
         </div>
 
         <div class="col-md" style="margin:5% 3% 5% 0%; text-align:center">
-            <h3>EXISTING CUSTOMER</h3>
-            <h6 class="mb-3">Welcome to SOUVENIR </h6>
-            <div class="container custom-login" style="padding:20px; background-color:#0097a7">
-                <form action="/login" method="POST">
-
+            <div class="container custom-login" style=" border-radius:20px; background-color:#0097a7">
+                <form action="login" method="POST">
                     <div class="form-floating mb-3">
                         @csrf
-                        <input type="email" value="" name="email" value="{{old('email')}}" class="form-control border-success" id="floatingInput" placeholder="name@example.com" required>
+                        <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
                         <label for="floatingInput">Email address</label>
-
-                        @isset($error)
-                        <span class="badge rounded-pill bg-danger fs-6 mt-2">{{$error}}</span>
-                        @endisset
-
+                       
                     </div>
                     <div class="form-floating">
-                        <input type="password" value="" name="password" class="form-control border-success" id="floatingPassword" placeholder="Password" required>
+                        <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
                         <label for="floatingPassword">Password</label>
-
-                        @isset($error1)
-                        <span class="badge rounded-pill bg-danger fs-6 mt-2">{{$error1}}</span>
-                        @endisset
-
                     </div>
                     <div class="form-floating mt-3">
-                        <button type="submit" style="width:100%; background-color:#00ba99; border: white solid 1px" class="btn btn-primary btn-lg btn-block">LOGIN</button>
+                        <button type="submit" style="width:100%; background-color:#00ba99; border: white solid 1px" class="btn btn-primary btn-lg btn-block">Login</button>
                     </div>
-                    <div class="mt-3">
-                        <a style="text-align:center; color:white" href="/"><span>Forgot password?</span></a>
-
+                    <div style="text-align:center; color:white" class="mt-3">
+                        <span>Forgot password?</span>
+                   
                         <hr>
                     </div>
+
+                           
                     <div style="text-align:center;">
-                        <a href="register"><button type="button" style="width:60%; background-color:#00b05f;border: white solid 1px" class="btn btn-primary btn-lg btn-block">REGISTER</button></a>
+                        <a href="register"><button type="button" style="width:60%; background-color:#00b05f;border: white solid 1px" class="btn btn-primary btn-lg btn-block">Create account</button></a>
                     </div>
+
+                  
+                     
                 </form>
+                
+              
+                @isset($error)
+                        <div class="card">
+                            <div class="text-danger">
+                                {{$error}}
+                            </div>
+                        </div>
+                @endisset
+
+
+
+
 
             </div>
         </div>
