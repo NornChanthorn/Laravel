b102ee6049fa81fa4b998bbe4c370422135d3235
Norn Chanthorn <nornchanthorn660@gmail.com>
2022-08-01 10:29:32 +0700

    add cart list

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -1,41 +1,55 @@
 @extends('master')
 @section('content')
-<div class="container custom-product " style="background-color:white">
+<div class="container custom-product ">
     <div class="col-sm-11">
         <div class="trending-wrapper">
 
             <h4>Your Cart List</h4>
-
-            <a class="btn btn-success" href="ordernow">Order Now</a>
-
-            <div class="container">
+      @if($product =="No order")
+         <div class="container p-5">
+             <h3 class="text-center">No Order Yet</h3>
+         </div>
+      
+      @else    
+        <div class="container">
+           
                 @foreach ($product as $item)
-                <div class="row search-item cart-list-divider">
+                  
+                <div class="row search-item cart_divider">
 
                     <div class="col mt-3">
                         <a href="detail/{{$item->id}}">
-                            <img class="trending-image" src="{{$item->gallery}}" class="d-block w-100" alt="...">
+                            <img class="trending-image" src="./image/{{$item->category}}/{{$item->gallery}}" class="d-block w-100" alt="...">
                         </a>
                     </div>
 
-                    <div class="col mt-2">
-                        <div class="">
-                            <h2 style="color:black">{{$item->name}}</h2>
-                            <h5 style="color:black">{{$item->description}}</h5>
+                    <div class="col mt-2 ">
+                        <div class="des">
+                            <h4 style="color:black">{{$item->name}}</h4>
+                            <h6 style="color:black">{{$item->description}}</h6>
                         </div>
                     </div>
 
-                    <div class="col mt-3">
-                        <a href="/removecart/{{$item->cart_id}}" class="btn btn-danger">Remove from cart</a>
+                    <div class="col mt-3 price">
+                       <h5 style="color: blue;"> $ {{$item->price}}</h5>
                     </div>
 
+                    <div class="col mt-3">
+                        <a href="/removecart/{{$item->cart_id}}" class="btn btn-danger remove">Remove from cart</a>
+                    </div>
+
+                    
                 </div>
                 @endforeach
-            </div>
+                <a class="btn btn-success btnOrder" href="ordernow">Order Now</a>
+             </div>
 
-            <a class="btn btn-success" href="ordernow">Order Now</a>
+          
             
         </div>
+        @endif
     </div>
 </div>
+</div>
 @endsection
+
