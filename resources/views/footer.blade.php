5a64ce771a74d8fc7a42a7b22cc14b7b2a2e061a
Soklieng <psoklieng@gmail.com>
2022-08-06 21:53:41 +0700

    footer style updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,111 +1,39 @@
-<footer class="bg-light text-center text-white">
-  <!-- Grid container -->
+<footer style="background-color:#1d7874 ;" class=" text-center text-white">
   <div class="container p-4 pb-0">
-    <!-- Section: Social media -->
     <section class="mb-4">
-      <!-- Facebook -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #3b5998;"
-        href="https://web.facebook.com/01pslieng"
-        role="button"
-        ><i class="fab fa fa-facebook"></i
-      ></a>
+      <div class="me-7 d-none d-lg-block mb-2">
+        <span class="text-center">Get connected with us on social networks:</span>
+      </div>
+      <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998;" href="https://web.facebook.com/01pslieng" role="button"><i class="fab fa fa-facebook"></i></a>
 
-      <!-- Twitter -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #55acee;"
-        href="#!"
-        role="button"
-        ><i class="fab fa fa-twitter"></i
-      ></a>
+      <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i class="fab fa fa-twitter"></i></a>
 
-      <!-- Google -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #dd4b39;"
-        href=""
-        role="button"
-        ><i class="fa fa-google"></i
-      ></a>
+      <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="" role="button"><i class="fa fa-google"></i></a>
 
-      <!-- Instagram -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #ac2bac;"
-        href="https://www.instagram.com/chanthorn9257/"
-        role="button"
-        ><i class="fa fa-instagram"></i
-      ></a>
-
-      <!-- Linkedin -->
+      <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="https://www.instagram.com/chanthorn9257/" role="button"><i class="fa fa-instagram"></i></a>
       
-      <!-- Github -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #333333;"
-        href="https://github.com/Chompanha"
-        role="button"
-        ><i class="fa fa-github"></i
-      ></a>
+      <a class="btn btn-primary btn-floating m-1" style="background-color: #333333;" href="https://github.com/Chompanha" role="button"><i class="fa fa-github"></i></a>
     </section>
-    <!-- Section: Social media -->
   </div>
-  <!-- Grid container -->
-
-
-  <!-- Footer -->
-<footer class="text-center text-lg-start bg-light text-muted">
-  <!-- Section: Social media -->
-  <section
-    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
-  >
-    <!-- Left -->
-    <div class="me-7 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <!-- Left -->
-
-    <!-- Right -->
-    
-    <!-- Right -->
-  </section>
-  <!-- Section: Social media -->
-
-  <!-- Section: Links  -->
+  <hr>
   <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <!-- Grid row -->
-      <div class="row mt-3">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
+    <div class="container text-center text-md-start mt-2">
+      <div class="row mt-2">
+        <div class="col-md-2 col-lg-2 col-xl-3 mx-auto">
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Souvenir Shop
+            Team member
           </h6>
-          <div class="member">
-            <p>
-            <h6 href="#!" class="fa fa-gem me-3">  Member</h6>
-            
+          <p>
+            Chom Panha
           </p>
           <p>
-            <a href="#!" class="text-reset">Chom Panha</a>
+            Phat Soklieng
           </p>
           <p>
-            <a href="#!" class="text-reset">Phat Soklieng</a>
+            Norn Chanthorn
           </p>
-          <p>
-            <a href="#!" class="text-reset">Norn Chanthorn</a>
-          </p>
-          </div>
-          
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
+        <div class="col-md-2 col-lg-2 col-xl-3 mx-auto">
           <h6 class="text-uppercase fw-bold mb-4">
             Products
           </h6>
@@ -122,15 +50,7 @@
             <a href="#!" class="text-reset">Others</a>
           </p>
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Links -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0">
           <h6 class="text-uppercase fw-bold mb-4">
             Contact
           </h6>
@@ -142,32 +62,11 @@
           <p><i class="fa fa-phone me-3"></i> + 855 97 830 2905</p>
           <p><i class="fa fa-print me-3"></i> + 855 96 353 4328</p>
         </div>
-        <!-- Grid column -->
       </div>
-      <!-- Grid row -->
     </div>
   </section>
-  <!-- Section: Links  -->
-
- 
-  <!-- Copyright -->
-
-<!-- Footer -->
-
-
-
-
-
-
-
-
-
-  <!-- Copyright -->
-
 
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2022 Copyright:
     <a class="text-white" href="https://mdbootstrap.com/">PSTSouvenirShop.com</a>
   </div>
-  <!-- Copyright -->
-</footer>
