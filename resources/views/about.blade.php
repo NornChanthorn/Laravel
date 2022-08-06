ef88ace6d9007f62fc640d477e47993a2452052f
Soklieng <psoklieng@gmail.com>
2022-08-06 23:09:34 +0700

    about us file updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 @section("content")
 
 <!-- Header -->
-<header class="bg-primary text-center py-5 mb-4">
+<header class="bg-info py-2 text-center  mb-4">
   <div class="container">
     <h1 class="fw-light text-white">Meet the Team</h1>
   </div>
@@ -14,7 +14,7 @@
     <!-- Team Member 1 -->
     <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="https://scontent.fpnh7-1.fna.fbcdn.net/v/t39.30808-6/277681024_1344623622631135_2031666048889774801_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFfqJSXcIskM6y87MlWm1Npk0-72rjJUXCTT7vauMlRcIajfcuxdU0g5BG0zQtXEGc4MtZ1-H1fZFP2qyITf-F7&_nc_ohc=TFi4ofxrb4MAX-JdM5X&tn=Igkl14fD-lRpBJ26&_nc_ht=scontent.fpnh7-1.fna&oh=00_AT-BItQlP68H4HgTGPe_PU5gPtte_EfKr_lgZUjY5PUWvA&oe=62F40C8B" height class="card-img-top" alt="...">
+        <img src="panha.jpg" height class="card-img-top" alt="...">
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Chom Panha</h5>
           <div class="card-text text-black-50">Team Leader</div>
@@ -23,9 +23,9 @@
       </div>
     </div>
     <!-- Team Member 2 -->
-    <div class="col-xl-4 col-md-6 mb-4">
+    <div class="col-xl-4 col-md-6 mb-4" >
       <div class="card border-0 shadow">
-        <img src="https://scontent.fpnh7-1.fna.fbcdn.net/v/t39.30808-6/278322334_535117181297567_5899361253188946869_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGFIBmLfKoYwFhl03HWmJnhv9glryxiXaG_2CWvLGJdoeuVH5EL93BrGtBMGxHaoXc0Q41AbbChHjobT91x4LG1&_nc_ohc=pLROFcWcvJ8AX_WAeyz&_nc_ht=scontent.fpnh7-1.fna&oh=00_AT_rVcKIhLdpQzCyiVjLS24NtuCx12T5Wr0h6EIO8xicZw&oe=62F38248" class="card-img-top" alt="...">
+        <img src="chanthorn.jpg" class="card-img-top" alt="...">
         <div class="card-body text-center">
         <h5 class="card-title mb-0">Norn Chanthorn</h5>
           <div class="card-text text-black-50">Team Member</div>
@@ -36,7 +36,7 @@
     <!-- Team Member 3 -->
     <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="r.jpg" class="card-img-top" alt="...">
+        <img src="soklieng.jpg" class="card-img-top" alt="...">
         <div class="card-body text-center">
         <h5 class="card-title mb-0">Phat Soklieng</h5>
           <div class="card-text text-black-50">Team Member</div>
